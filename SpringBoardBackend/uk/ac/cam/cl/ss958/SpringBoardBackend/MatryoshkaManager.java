54f811143c7af90ddb0f4be036cc63834837bf8b
Szymon Sidor <szymon.sidor@gmail.com>
2012-12-20 15:24:44 +0100

    Initial commit.
    Simulation GUI almost done:
    -> can move around Users
    -> can display user options
    -> can generate and remove users

---
+++ SpringBoardBackend/uk/ac/cam/cl/ss958/SpringBoardBackend/MatryoshkaManager.java
@@ -0,0 +1,10 @@
+package uk.ac.cam.cl.ss958.SpringBoardBackend;
+
+public class MatryoshkaManager {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SpringBoardSimulation/uk/ac/cam/cl/ss958/SpringBoardSimulation/Strings.java
@@ -0,0 +1,9 @@
+package uk.ac.cam.cl.ss958.SpringBoardSimulation;
+
+
+public class Strings {
+	public static final String PANEL_SIMULATION = "Simulation";
+	public static String PANEL_USEROPT = "User Options";
+	public static String PANEL_GLOBALOPT = "Simulation Options";
+
+}
